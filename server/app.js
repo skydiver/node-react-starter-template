646747808615c7d4c39cdbb4335cdfd12a6cc3ab
Martin <skydiver@users.noreply.github.com>
2018-08-10 01:10:12 -0300

    [server] linting + es6

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -22,18 +22,18 @@ app.set('view engine', 'pug');
 
 app.use('/api', index);
 app.get('*', (req, res) => {
-  res.sendFile('build/index.html', { root: global });
+  res.sendFile('build/index.html', {root: global});
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/message', function(req, res, next) {
+router.get('/message', function (req, res, next) {
   res.json('Welcome To React');
 });
 
