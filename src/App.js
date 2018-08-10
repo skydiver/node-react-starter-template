a9e9bd66e5bdf4322c58505b22847d0fc3a1642a
Martin <skydiver@users.noreply.github.com>
2018-08-10 16:48:59 -0300

    [app] cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { message: '' };
+    this.state = {message: ''};
   }
 
   componentDidMount() {
     fetch('/api/message')
       .then(response => response.json())
-      .then(json => this.setState({ message: json }));
+      .then(json => this.setState({message: json}));
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>{this.state.message}</h2>
         </div>
         <p className="App-intro">
